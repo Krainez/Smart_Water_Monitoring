45aec98f1fbf9938313a7b225f867d1da979f4c6
Krainez <30413875+Krainez@users.noreply.github.com>
2019-08-23 16:39:57 +0300

    Add files via upload

---
+++ arduino_part.ino
@@ -0,0 +1,40 @@
+// defines pins numbers
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzzer=3;
+// defines variables
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(buzzer,OUTPUT);
+Serial.begin(115200); // Starts the serial communication
+}
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+if (distance > 12 )
+{
+  tone(buzzer,2000,50);
+  
+}
+if (distance <=3){
+  tone(buzzer,4000,50);
+}
+
+// Prints the distance on the Serial Monitor
+delay(1);
+
+Serial.println(distance);
+}
---
+++ nodemcu_part.ino
@@ -0,0 +1,76 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+
+#define WIFI_SSID "VINNWiFi_7365"
+#define WIFI_PASSWORD "80005959"
+#define FIREBASE_HOST "iot-water-monitoring.firebaseio.com"
+#define FIREBASE_AUTH "Z63iYbiH37fMABwCi8J2FUreVnL7ochUacuFhcYE"
+
+
+int timezone=3*3600;
+int dst=0;
+String t;
+
+void setup() {
+   // Initialize the serial port
+  Serial.begin(115200);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
+  Serial.println("\nWaiting for Internet time");
+
+  while(!time(nullptr)){
+     Serial.print("*");
+     delay(1000);
+  }
+  Serial.println("\nTime response....OK"); 
+
+}
+
+void loop() {
+
+delay(1000);
+int i=0;
+ while(Serial.available() > 0) {
+    delay(3000);
+    t = Serial.read();
+    Serial.println(t);
+    Firebase.setString("WaterLevel/Percentage", t);                                  //setup path and send readings  
+    i++;
+    if(i==20)
+    {
+      time_t timepast = now();
+      Firebase.pushString("PastWaterLevel/Percent",t); 
+      Firebase.pushString("PastWaterLevel/Time",String(timepast));   
+      i=0;
+    }
+    if (Firebase.failed()) { 
+     Serial.print("pushing WaterLevel failed:"); 
+     Serial.println(Firebase.error());   
+     return; 
+    }else
+    {
+      Serial.println("Transmission finised");
+    }
+    
+  }
+  
+
+  // Send a message over the serial port
+ 
+
+}
